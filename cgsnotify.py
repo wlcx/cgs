3b0162b29b7b368afb09f6f0a3b0aa9b0b7a28f1
Sam Willcocks <sam@wlcx.cc>
2013-07-07 01:45:06 +0100

    PEP8 cleanup
    Compulsive formatting!

--- cgsnotify.py
+++ cgsnotify.py
@@ -13,7 +13,7 @@ except ImportError:
     logging.ERROR("No config file found")
     sys.exit()
 
-#Post notification to pushover servers
+# Post notification to pushover servers
 def notify(userkey, title, message):
     conn = httplib.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
@@ -30,11 +30,11 @@ def listLoggedInUsers():
     users = []
     for x in s.getUsers(): # x is key for dictioary s.getUsers()
         users.append(s.getUsers()[x].name)
-    #WIP: Possible Alternative
-    #WIP: users=[s.getUsers()...
+    # WIP: Possible Alternative
+    # WIP: users = [s.getUsers()...
     return users
 
-#Format a list with nice grammar (so ['foo', 'bar', 'baz'] returns 'foo, bar and baz')
+# Format a list with nice grammar (so ['foo', 'bar', 'baz'] returns 'foo, bar and baz')
 def prettyPrintList(inlist):
     outstring = ""
     numUsers=len(inlist)
@@ -42,7 +42,7 @@ def prettyPrintList(inlist):
         outstring += inlist[0]
     if numUsers == 2: # foo and bar
         outstring += (inlist[0] + " and " + inlist[1])
-    if numUsers >= 3: #foo, bar and baz
+    if numUsers >= 3: # foo, bar and baz
         for x in range(numUsers-2):
             outstring += inlist[x] + ", "
         # exploits wraparound indexing, s[-1] refers to last item
@@ -60,7 +60,7 @@ if __name__ == '__main__':
  
     logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s',datefmt='%d/%m/%y %H:%M:%S',level=logging.DEBUG)
     
-    #retrieve 1st virtual server object
+    # retrieve 1st virtual server object
     s = mice.m.getServer(1)
     
     while True:
@@ -69,32 +69,32 @@ if __name__ == '__main__':
             time.sleep(5)
             currentusers = listLoggedInUsers()
 
-            # Will show new users, so if oldusers=["a","b","c","d"],
-            # currentusers=["a","e","f"], newusers=["e","f"]
+            # Will show new users, so if oldusers = ["a","b","c","d"],
+            # currentusers = ["a","e","f"], newusers = ["e","f"]
             newusers = list(set(currentusers) - set(oldusers))
             if len(newusers) > 0:
                 logging.info('%s logged in!', prettyPrintList(newusers))
 
                 # Only works for non-zero users (but in if so its fine)
-                isare=isorare[bool(len(currentusers) - 1)]
-                #Alternative:
-                ##if len(currentusers) == 1: isare = 'is'
-                ##else: isare = 'are'
+                isare = isorare[bool(len(currentusers) - 1)]
+                # Alternative:
+                # if len(currentusers) == 1: isare = 'is'
+                # else: isare = 'are'
                 
-                #Output below
+                # Output below
                 if args.test_mode:
                     logging.info('Running in testing mode')
                     notify(users[args.test_mode], ("TESTING:" + prettyPrintList(newusers) + " logged in"), 
                                    prettyPrintList(currentusers) + " " + isare + " online.")
                 else:
-                    for x in users.keys(): #list of names for those with pushover
+                    for x in users.keys(): # list of names for those with pushover
                         if x in currentusers:
                             logging.info("%s is logged in already, skipping", x)
                         else:
                             logging.info("Notifying %s", x)
                             notify(users[x],(prettyPrintList(newusers) + " logged in"), 
                             (prettyPrintList(currentusers) + " " + isare + " online."))
-                            time.sleep(1) #be nice to the api
+                            time.sleep(0.5) # be nice to the api
         except KeyboardInterrupt:
             logging.info("Caught SIGINT, exiting")
             sys.exit()
